ea9febdd771f11076dc2020c1f27021d12bce4dd
DarthRomolus <rom.mail2005@gmail.com>
2025-08-05 22:47:50 +0300

    forgot to save 2 files with routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import type {Request ,Response} from 'express';
+import mainRoutes from './routes/mainRoutes'
 
 let app=express();
 const PORT=3000;
 app.use(express.json());
+app.use('/game',mainRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
