af021b7d524ed5fc50eeb447326708940ce5bfb0
DarthRomolus <rom.mail2005@gmail.com>
2025-08-05 22:13:17 +0300

    feat: added routes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-let app = require('express')()
+import express from 'express';
+import type {Request ,Response} from 'express';
+
+let app=express();
+const PORT=3000;
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+app.get("/",(req: Request,res: Response)=>{
+    console.log("getting");
+    res.send("hi");
+})
---
+++ src/routes/gameRoutes.ts
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+
+const router = Router();
+router.use('/start',);
+router.use('/stand',);
+router.use('/hit',);
+router.use('/end',)
+
+export default router;
