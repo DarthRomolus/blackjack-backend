4e942f5bb31191ae114eeb40f050d97ce85f3c11
DarthRomolus <rom.mail2005@gmail.com>
2025-08-17 18:43:20 +0300

    prittier

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import cors from "cors";
 let app = express();
 const PORT = 3000;
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5173 " }));
 app.use("/game", mainRoutes);
 
 app.listen(PORT, () => {
